c71511b06fa7279716cc3bde5ff27c4eabb48624
shraddhabhatt <trivedishraddha99@gmail.com>
2018-03-14 16:43:13 -0400

    Commited the ReadMe and Video link

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Bamazon
+
+Created during Week 12 of UNH Coding Bootcamp. The goal was to create an Amazon-like store front using Node.js and MySQL.
+
+## Getting Started
+
+- Clone repo.
+- Run command in Terminal or Gitbash 'npm install'
+- Run 'ctrl + c' to exit each mode
+
+### What Each JavaScript Does
+
+1. `BamazonCustomer.js`
+
+    * Prints the products in the store.
+
+    * Prompts customer which product they would like to purchase by ID number.
+
+    * Asks for the quantity.
+
+      * If there is a sufficient amount of the product in stock, it will return the total for that purchase.
+      * However, if there is not enough of the product in stock, it will tell the user that there isn't enough of the product.
+      * If the purchase goes through, it updates the stock quantity to reflect the purchase.
+      * It will also update the product sales in the department table.
+
+    * Demo Video (https://youtu.be/NPdy_rc1Zhc)
+-----------------------
+
+## Technologies used
+- Node.js
+- Inquire NPM Package (https://www.npmjs.com/package/inquirer)
+- MYSQL NPM Package (https://www.npmjs.com/package/mysql)
+
+### Prerequisites
+
+```
+- Node.js - Download the latest version of Node https://nodejs.org/en/
+- Create a MYSQL database called 'Bamazon', reference schema.sql
+```
+
+## Built With
+
+* Sublime Text - Text Editor
+* MySQLWorkbench
+* Terminal/Gitbash
+
+## Authors
+
+* **Shraddha Bhatt** - *JS/MySQL/Node.js* - [Shraddha Bhatt](https://github.com/shraddhabhatt/Bamazon)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,7 +14,7 @@ connection.connect(function(err) {
    if (err) {
        throw err;
    }
-   console.log("Connected as Id: " + connection.threadId);
+   //console.log("Connected as Id: " + connection.threadId);
 });
 
 var inq = require('inquirer');
@@ -40,8 +40,109 @@ var displayProductList = function(){
             productlist.push([data.item_id, data.product_name, data.department_name, data.price, data.stock_quantity]);
         }
         console.log(productlist.toString() + "\n\n");
+        getOrder();
+    });
+    
+}
+displayProductList();
+
+var getOrder = function(){
+       inq.prompt([{
+           type: "choices",
+           message: "Which product would you like to purchase?? Please enter an item_id \n\n",
+           name: "item"
+       }]).then(function(data) {
+           //take entry -> make number
+           data.item = parseInt(data.item);
+           //check if entry is a number
+           if (isNaN(data.item) === false) {
+               var item = data.item;
+               connection.query("select max(item_id) as max from products;", function(err, result, fields) {
+                    if (err) throw err;
+                    
+                    if(result[0].max >= item)
+                        getQuantity(item); //now ask how much they want
+                    else{
+                        console.log("\nItem ID does not match\n");
+                        getOrder();
+                    }
+                });
+               
+           }
+           //if NaN have user try again
+           else {
+               console.log("\nPLEASE ENTER A NUMBER\n");
+               getOrder();
+            }
+        });
+}
+    
+var getQuantity = function(item) {
+    inq.prompt([{
+        type: "input",
+        message: "How many would you like to purchase?? Please enter number \n\n",
+        name: "count"
+    }]).then(function(data) {
+        //take entry -> make number
+        data.count = parseInt(data.count);
+        //check if entry is a number
+        if (isNaN(data.count) === false) {
+            var quant = parseInt(data.count);
+            confirmOrder(item, quant);
+         }
+         //if NaN have user try again
+         else {
+            console.log("PLEASE ENTER A NUMBER");
+            getQuantity(item);
+        }
+    });
+}
+//verify order details
+var confirmOrder = function(item, quant) {
+
+    var query = "SELECT product_name, price, stock_quantity FROM products WHERE ?";
+    connection.query(query, { item_id: item }, function(err, res) {
+
+    var custCost = quant * res[0].price;
+    var response = "";
+    if (quant <= res[0].stock_quantity) {
+        inq.prompt({
+            name: "confirmOrder",
+            type: "confirm",
+            message: "Please confirm you want to purchase " + quant + " " + res[0].product_name + " for $" + custCost
+        }).then(function(answer) {
+            if (answer.confirmOrder === true) {
+                    response = "\nAwesome! We are processing your order!\n.....\n.....\n.....\n.....\n.....\n";
+                    var quantNew = res[0].stock_quantity - quant;
+                    updateDB(item, quantNew);
+                } else {
+                console.log("\nOkay. See ya later");
+                stopDb();
+                }
+            });
+        }else{
+                response = "\nSorry, but you've requested more " + res[0].product_name + " than we have available.\n\n";
+                console.log(response);
+                stopDb();
+        }
+             
     });
 }
 
-displayProductList();
-
+//update the database to reflect confirmed order
+var updateDB = function(item, quantNew) {
+    connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: quantNew},{item_id: item}],
+        function(err) {
+            if (err) throw err;
+            console.log("The database has been updated!");
+            stopDb();
+         }
+    )
+}
+//end db connection
+var stopDb = function() {
+    connection.end(function(err) {
+        if (err) { throw err; }
+         console.log("Disconnected from database!\n\n");
+    });
+}
